7b985840746867dffab1ba8f9319c83f91f95e01
shahid <shahidazhari313@gmail.com>
2024-02-25 18:42:32 +0530

    update user api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
+
+
 dotenv.config();
 
 const connectDB = async () => {
@@ -19,7 +22,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser())
 app.listen(4000, () => {
   console.log(`Server running on http://localhost:4000/`);
 });
