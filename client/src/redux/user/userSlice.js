6fabd6816076a9c3274fb4cbaf173ee246ba18c3
shahid <shahidazhari313@gmail.com>
2024-02-25 21:55:57 +0530

    user updated

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -22,8 +22,28 @@ const userSlice = createSlice({
       state.loading = false;
       state.error = action.payload;
     },
+    updateUserStart: (state) => {
+      state.loading = true;
+    },
+    updateUserSuccess: (state, action) => {
+      state.currentUser = action.payload;
+      state.loading = false;
+      state.error = false;
+    },
+    updateUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInFailure, signInStart, signInSuccess } = userSlice.actions;
-export default userSlice.reducer
+export const {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+  updateUserFailure,
+  updateUserSuccess,
+  updateUserStart,
+} = userSlice.actions;
+
+export default userSlice.reducer;
